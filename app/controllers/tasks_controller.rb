aea164d155e41faddc8cfe23b30373f9c9c281af
Ailin Huang <ailin.h58@gmail.com>
2020-05-19 15:10:39 +0200

    task manager completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    # get form
+  end
+
+  def update
+    # find
+    # update
+    @task.update(task_params)
+    # redirect to updated task
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # find
+    # delete
+    @task.destroy
+    # redirect
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
